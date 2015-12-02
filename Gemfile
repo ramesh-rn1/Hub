f7dc51020a70a3f580c21a0d84ae6d330c519f3a
Ramesh <ramesh.nagaraj21@gmail.com>
2015-12-02 11:23:30 +0530

    Add and setup voting

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'devise', '~> 3.5', '>= 3.5.2'
 
 gem 'paperclip', '~> 4.3', '>= 4.3.2'
 
+gem 'acts_as_votable', '~> 0.10.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	has_attached_file :image, styles: { medium: "700x500>", small: "350x250>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  	acts_as_votable
 end
